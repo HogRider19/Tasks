1c1b3ae53e8207e1c54cf3fee7aaa7c0a0b0528b
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-12-09 15:50:44 +0300

    !

---
+++ NumbersThatArePowerOfTheirSumOfDigits.py
@@ -0,0 +1,16 @@
+"""
+The number 81 has a special property, a certain power of the sum of
+its digits is equal to 81 (nine squared). Eighty one (81), is the first
+number in having this property (not considering numbers of one digit).
+The next one, is 512. Let's see both cases with the details
+We need to make a function that receives a number as argument
+n and returns the n-th term of this sequence of numbers.
+"""
+
+def power_sumDigTerm(n: int):
+    return
+
+
+assert power_sumDigTerm(1) == 81
+assert power_sumDigTerm(2) == 512
+assert power_sumDigTerm(3) == 2401
