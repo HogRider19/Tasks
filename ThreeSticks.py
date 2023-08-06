7df9656514a81442cb6e1740f3aa2d27467d3a1f
HogRider <danilzzzg19@gmail.com>
2023-08-06 16:54:49 +1000

    !

---
+++ ThreeSticks.py
@@ -0,0 +1,11 @@
+"""
+    Imagine that you are given two sticks. You want to end up with three sticks of equal length.
+    You are allowed to cut either or both of the sticks to accomplish this, and can throw away leftover pieces.
+    Write a function, maxlen, that takes the lengths of the two sticks (L1 and L2, both positive values),
+    that will return the maximum length you can make the three sticks.
+"""
+
+
+def maxlen(l1, l2):
+    return max(min(l1 / 2, l2), min(l1, l2 / 2), max(l1, l2) / 3)
+
